aaf48101ddc6f37c84d6718c7d36d3e6f4372053
tulioemmanuel <tulioemmanuel@hotmail.com>
2019-02-04 22:46:08 -0200

    inicio

--- main.cpp
+++ main.cpp
@@ -2,14 +2,33 @@
 #include <tchar.h>
 #include <Python.h>
 
+#define CMD_ARGS TRUE
+
+void PreferenciasRuntime(LPSTR);
+BOOL IniciarConsolePython();
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
 {
+#if CMD_ARGS
+    PreferenciasRuntime(pCmdLine);
+#endif
+}
 
-    PyObject* pInt;
+void PreferenciasRuntime(LPSTR args)
+{
+
+    // Se forem iguais retorna 0
+    if (!lstrcmp(TEXT(args), TEXT("debug")))
+        IniciarConsolePython();
+}
+
+BOOL IniciarConsolePython()
+{
+
+    PyObject *pInt;
     Py_Initialize();
     pInt = PyString_FromString("teste");
-
-    MessageBox(NULL, _T("TESTE"), _T("TESTE"), MB_OK);
-
     Py_Finalize();
+
+    return TRUE;
 }
